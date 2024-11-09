38a714dab9fa85976904fb26e5363e518e3fae40
Tom English <tom@martee.ai>
2024-11-09 12:47:45 +0000

    Plan crafting complete

--- scripts/gui/intel_list.gd
+++ scripts/gui/intel_list.gd
@@ -11,6 +11,9 @@ extends Window
 @onready var when_description = find_child("WhenDescription")
 
 @onready var create_plan_btn = find_child("CreatePlanBtn")
+@onready var plan_list_container = find_child("PlanListContainer")
+@onready var tab_container = find_child("TabContainer")
+
 
 # Variables to store selected intel nodes
 var selected_intel = {
@@ -23,18 +26,32 @@ var selected_intel = {
 func _ready():
 	# Connect the close_requested signal
 	close_requested.connect(_on_close_requested)
-	
+
 	create_plan_btn.visible = false
 	create_plan_btn.pressed.connect(_on_create_plan_btn_pressed)
+	
+	# Connect the tab change signal
+	tab_container.tab_changed.connect(_on_tab_changed)
 	
 	# Fetch intel from the GlobalRegistry and populate the list
 	var intel = GlobalRegistry.get_all_objects(GlobalRegistry.Registry_Category.INTEL)
 	populate_intel_list(intel)
-	
+	populate_plan_list(intel)
+
 func _on_close_requested():
 	queue_free()
+	
+func clear_container(container):
+	while container.get_child_count() > 0:
+		container.remove_child(container.get_child(0))
 
 func populate_intel_list(intel):
+	# Clear any existing controls in the containers
+	clear_container(who_btn_grp_container)
+	clear_container(what_btn_grp_container)
+	clear_container(where_btn_grp_container)
+	clear_container(when_btn_grp_container)
+	
 	var intel_type_to_container = {
 		Enums.IntelType.WHO: { "container": who_btn_grp_container, "button_group": ButtonGroup.new(), "description_label": who_description },
 		Enums.IntelType.WHAT: { "container": what_btn_grp_container, "button_group": ButtonGroup.new(), "description_label": what_description },
@@ -69,6 +86,18 @@ func populate_intel_list(intel):
 		else:
 			print("Error: Container not found or intel_data is invalid for:", name, "with type:", intel_node.type)
 
+func populate_plan_list(intel):
+	# Clear any existing elements in the plan_list_container
+	clear_container(plan_list_container)
+
+	# Loop over intel and add labels for nodes with level PLAN
+	for name in intel.keys():
+		var intel_node = intel[name]
+		if intel_node.level == Enums.IntelLevel.PLAN:
+			var label = Label.new()
+			label.text = intel_node.description
+			plan_list_container.add_child(label)
+
 func _check_create_plan_visibility():
 	# Check if all intel types have a selected intel node
 	if selected_intel[Enums.IntelType.WHO] and selected_intel[Enums.IntelType.WHAT] and selected_intel[Enums.IntelType.WHERE] and selected_intel[Enums.IntelType.WHEN]:
@@ -84,7 +113,32 @@ func _on_create_plan_btn_pressed():
 		selected_intel[Enums.IntelType.WHERE],
 		selected_intel[Enums.IntelType.WHEN]
 	])
+
+	# Switch to the 'Plans' tab
+	tab_container.current_tab = 1
+
+func _on_tab_changed(tab_index):
+	# Re-run both populate_plan_list and populate_intel_list when the tab changes
+	var intel = GlobalRegistry.get_all_objects(GlobalRegistry.Registry_Category.INTEL)
+	_reset()
+	populate_intel_list(intel)
+	populate_plan_list(intel)
 	
-	#TODO: Probably this should switch to the 'Plans' tab OR refresh the layout
-	# Close the window
-	_on_close_requested()
+func _reset():
+	selected_intel = {
+		Enums.IntelType.WHO: null,
+		Enums.IntelType.WHAT: null,
+		Enums.IntelType.WHERE: null,
+		Enums.IntelType.WHEN: null
+	}
+	
+	who_description.text = ""
+	what_description.text = ""
+	where_description.text = ""
+	when_description.text = ""
+	
+	create_plan_btn.visible = false
+	
+	
+	
+	
--- scripts/map.gd
+++ scripts/map.gd
@@ -20,10 +20,10 @@ func _ready() -> void:
 		var character = CharacterFactory.create_character()
 		$CanvasLayer/Map_Panel_Left.add_character_card(character.id)
 	
-		var whoR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(100,0,0,0))
-		var whereR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,100,0,0))
-		var whatR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,0,100,0))
-		var whenR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,0,0,100))
+		#var whoR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(100,0,0,0))
+		#var whereR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,100,0,0))
+		#var whatR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,0,100,0))
+		#var whenR = IntelFactory.create_rumour(IntelFactory.RumourConfig.new(0,0,0,100))
 
 	#var plan = IntelFactory.combine_rumours([whoR, whereR, whatR, whenR])
 	
