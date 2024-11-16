c858c3b248d8c9eb7a8aa8b2759911822b7435ce
Tom English <tom@martee.ai>
2024-11-16 09:56:04 +0000

    Espionage action logic

--- scripts/actions/espionage_action.gd
+++ scripts/actions/espionage_action.gd
@@ -5,7 +5,33 @@ class_name EspionageAction
 
 func _process_action() -> Array[String]:
 	var logs: Array[String] = []
+	var log_message: String = ""
 
 	logs.append("Processing ESPIONAGE action at [u]" + str(poi.poi_name) + "[/u] by " + str(characters))
+
+	# Get cumulative stats for all characters involved
+	var stats: Dictionary = _get_stats()
+
+	var stat_check:String = ""
+	match poi.stat_check_type:
+		Enums.StatCheckType.SMARTS:
+			stat_check = "smarts"
+		Enums.StatCheckType.CHARM:
+			stat_check = "charm"
+
+	var roll = MathHelpers.bounded_sigmoid_check(stats[stat_check], true)
+		
+	if(roll.success):
+		log_message = "Succeeded " + stat_check + " check..."
+		logs.append(log_message)
+
+		var rumour: Rumour = IntelFactory.create_rumour(poi.rumour_config)
+
+		log_message = "Generated intel of type " + str(rumour.rumour_type)
+		logs.append(log_message)
+
+	else:
+		log_message = "Failed " + stat_check + " check..."
+		logs.append(log_message)
 
 	return logs
