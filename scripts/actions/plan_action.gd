fced86202178f6b317666aaef98fe7c2649bc676
Tom English <tom@martee.ai>
2024-11-16 12:24:22 +0000

    Build sympathy all plan

--- scripts/actions/plan_action.gd
+++ scripts/actions/plan_action.gd
@@ -60,6 +60,25 @@ func _build_sympathy_all() -> Array[String]:
 	var logs: Array[String] = []
 	var log_message: String = ""
 
+	# Get cumulative stats for all characters involved
+	var stats: Dictionary = _get_stats()
+
+	var charm_roll = MathHelpers.bounded_sigmoid_check(stats["charm"], true)
+		
+	if(charm_roll.success):
+		log_message = "Succeeded charm check..."
+		logs.append(log_message)
+
+		for my_poi in GlobalRegistry.poi.find_all_items(GlobalRegistry.LIST_POI, "parent_district", poi.parent_district):
+			my_poi.poi_owner.char_sympathy += Constants.ACTION_EFFECT_PLAN_BUILD_SYMPATHY_ALL_MODIFIER
+
+			log_message = my_poi.poi_owner.char_full_name + " sympathy increased by " + str(Constants.ACTION_EFFECT_PLAN_BUILD_SYMPATHY_ALL_MODIFIER)
+			logs.append(log_message)
+
+	else:
+		log_message = "Failed charm check..."
+		logs.append(log_message)
+
 	return logs
 
 func _discover_all() -> Array[String]:
