a682757e165ea919a3e9b2f86733fb7f1b860b77
Tom English <tom@martee.ai>
2024-11-16 09:48:25 +0000

    Surveillance action logic

--- scripts/actions/surveillance_action.gd
+++ scripts/actions/surveillance_action.gd
@@ -5,7 +5,26 @@ class_name SurveillanceAction
 
 func _process_action() -> Array[String]:
 	var logs: Array[String] = []
+	var log_message: String = ""
 
 	logs.append("Processing SURVEILLANCE action at [u]" + str(poi.poi_name) + "[/u] by " + str(characters))
+
+	# Get cumulative stats for all characters involved
+	var stats: Dictionary = _get_stats()
+
+	var smarts_roll = MathHelpers.bounded_sigmoid_check(stats["smarts"], true)
+		
+	if(smarts_roll.success):
+		log_message = "Succeeded smarts check..."
+		logs.append(log_message)
+
+		poi.poi_owner.char_recruitment_state = Enums.CharacterRecruitmentState.NON_SYMPATHISER_KNOWN
+
+		log_message = poi.poi_owner.char_full_name + " is now known to us"
+		logs.append(log_message)
+
+	else:
+		log_message = "Failed smarts check..."
+		logs.append(log_message)
 
 	return logs
