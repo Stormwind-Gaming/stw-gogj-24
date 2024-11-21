660c078b0ab90e0dc5524ad44544a126b4f57e84
TinTuna <rob.english@live.com>
2024-11-21 17:22:15 +0000

    feat: Issue#25 add stats panel to game over screen

--- scripts/gui/game_over.gd
+++ scripts/gui/game_over.gd
@@ -1,6 +1,14 @@
 extends Control
 
 #|==============================|
+#|   Exported Properties       |
+#|==============================|
+"""
+@brief Reference to the stats panel
+"""
+@export var stats_panel: PanelContainer
+
+#|==============================|
 #|        Lifecycle methods     |
 #|==============================|
 """
@@ -10,6 +18,9 @@ func _ready() -> void:
 	print("Game Over Screen Ready")
 	$AnimationPlayer.play("fade_in")
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open_stats_panel"):
+		stats_panel.visible = not stats_panel.visible
 
 #|==============================|
 #|        Signal Callbacks      |
