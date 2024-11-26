982dbf6b11956b3b2a5273d612b56557c5878a3a
Tom English <tom@martee.ai>
2024-11-26 20:42:44 +0000

    TurnLog debug logs

--- scripts/log/turn_log.gd
+++ scripts/log/turn_log.gd
@@ -8,9 +8,17 @@ var log_message: String = ""
 var log_type: Enums.LogType
 
 func _init(message: String, type: Enums.LogType, poi_attr: PointOfInterest = null, agents_attr: Array[Character] = []) -> void:
+	LogDuck.d("Creating new turn log", {
+		"message": message,
+		"type": type,
+		"poi": poi_attr.poi_name if poi_attr else "None",
+		"agents": agents_attr.map(func(a): return a.get_full_name()) if agents_attr else []
+	})
+	
 	poi = poi_attr
 	agents = agents_attr
 	log_message = message
 	log_type = type
 
 	EventBus.log_created.emit(self)
+	LogDuck.d("Turn log created and emitted")
