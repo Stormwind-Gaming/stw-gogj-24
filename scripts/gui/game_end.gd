3a091fff9cdf0b0eac83be5725621fc4fcd0d2be
TinTuna <rob.english@live.com>
2024-11-26 18:56:42 +0000

    feat: endgame event images

---
+++ scripts/gui/game_end.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+#|==============================|
+#|   Exported Properties       |
+#|==============================|
+
+"""
+This script is used to control the game end screen.
+"""
+@export var left_panel: TextureRect
